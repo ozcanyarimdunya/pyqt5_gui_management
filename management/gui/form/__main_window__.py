b332501ec104d94f9053a281c5659f482d9be86e
ozcanyarimdunya <ozcanyd@gmail.com>
2018-05-13 03:02:40 +0300

    eventFilter for status message

--- management/gui/form/__main_window__.py
+++ management/gui/form/__main_window__.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import QEvent
 from PyQt5.QtWidgets import QMainWindow, QSplitter, QFrame
 
 from management.gui.form.generated.main import Ui_MainWindow
@@ -14,11 +15,13 @@ class MainForm(QMainWindow, Ui_MainWindow):
         self.middle_form = MiddleWidget(parent=self)
         self.left_form = LeftWidget(parent=self)
 
+        self.left_form.installEventFilter(self)
+        self.middle_form.installEventFilter(self)
+
         self.initialise()
         self.configure()
 
     def initialise(self):
-        self.statusbar.showMessage("Select project to edit, delete or configure.")
         splitter = QSplitter()
         self.left_form.setFrameShape(QFrame.StyledPanel)
         self.middle_form.setFrameShape(QFrame.StyledPanel)
@@ -34,3 +37,11 @@ class MainForm(QMainWindow, Ui_MainWindow):
         self.middle_form.bind_edit_dialog(self.middle_form.dialog)
         self.left_form.bind_add_dialog(self.left_form.dialog)
         self.left_form.bind_edit_dialog(self.middle_form.dialog)
+
+    def eventFilter(self, obj, event):
+        if event.type() == QEvent.Enter:
+            if obj == self.left_form:
+                self.statusbar.showMessage("Select project to go second window.")
+            if obj == self.middle_form:
+                self.statusbar.showMessage("Select project to edit, delete or configure.")
+        return super().eventFilter(obj, event)
